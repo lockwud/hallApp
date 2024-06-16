e4ab34261cc1b016b0e31f1b58da0ee409330822
python <oklement3@gmail.com>
2024-06-16 02:03:07 +0000

    created httpstatus code and added a create function for student and defined the types on the student schema using zod

--- dist/src/utils/zodSchema.js
+++ dist/src/utils/zodSchema.js
@@ -1,1 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.studentSchema = void 0;
+const zod_1 = __importDefault(require("zod"));
+exports.studentSchema = zod_1.default.object({
+    studentId: zod_1.default.string(),
+    profile: zod_1.default.string(),
+    fullName: zod_1.default.string(),
+    email: zod_1.default.string().email(),
+    password: zod_1.default.string().min(8),
+    gender: zod_1.default.string(),
+    level: zod_1.default.number(),
+    telephone: zod_1.default.string().length(10)
+});
+module.exports = exports.studentSchema;
--- src/utils/zodSchema.ts
+++ src/utils/zodSchema.ts
@@ -1,1 +1,15 @@
 import zod from "zod"
+
+export const studentSchema = zod.object({
+    studentId: zod.string(), 
+    profile: zod.string(),    
+    fullName: zod.string(),
+    email: zod.string().email(),
+    password: zod.string().min(8),
+    gender: zod.string(),
+    level: zod.number(),    
+    telephone: zod.string().length(10)
+})
+
+
+module.exports = studentSchema;
