cd655ea9b2b4d498ad3b88ab50c288b2eb35affb
python <oklement3@gmail.com>
2024-06-22 04:02:15 +0000

    added roomRequest Router to index route as a middleware

--- src/routers/v1/index.ts
+++ src/routers/v1/index.ts
@@ -5,10 +5,13 @@ import roomRouter from "./roomsRoute"
 const studentRouter = router;
 import hall from "./hall.router"
 const mainRouter = Router();
+import requestRouter from "./roomRequest"
+
 
 mainRouter.use("/admin", adminRouter);
 mainRouter.use("/student", studentRouter)
 mainRouter.use("/room", roomRouter)
 mainRouter.use("/hall",hall)
+mainRouter.use("request", requestRouter )
 
 export default mainRouter;
